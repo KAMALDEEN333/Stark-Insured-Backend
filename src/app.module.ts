de60e4a0221a93c8cec714405b0228ab9f1af481
OthmanImam <imkargi@gmail.com>
2026-01-22 14:42:02 +0100

    Implemented Policy Lifecycle State Machine

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from './config/config.module';
 import { HealthModule } from './modules/health/health.module';
+import { PolicyModule } from './modules/policy/policy.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule, HealthModule],
+  imports: [ConfigModule, HealthModule, PolicyModule],
   controllers: [AppController],
   providers: [AppService],
 })
